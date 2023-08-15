22aa35d0b540395e7898240790cb06776fe0b20b
Daviti Vashakmadze <vashaymadzedavit@gmail.comgit config --global user.name Datuchaaa>
2023-08-15 21:18:14 +0400

    init: initialize project

---
+++ src/pages/statistics.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Box } from '@mui/system';
+
+const statistics = () => {
+  return (
+    <div>
+      <Box
+        sx={{
+          borderColor: 'neutral.300',
+          borderStyle: 'dashed',
+          borderWidth: 1,
+          height: 300,
+          p: '4px',
+        }}
+      />
+    </div>
+  );
+};
+
+export default statistics;
---
+++ src/routes/index.tsx
@@ -0,0 +1,34 @@
+import { Suspense } from 'react';
+import type { RouteObject } from 'react-router';
+import { Outlet } from 'react-router-dom';
+
+import { Layout as DashboardLayout } from 'src/layouts/dashboard';
+import HomePage from 'src/pages';
+import BlankPage from 'src/pages/blank';
+import Satistcs from 'src/pages/statistics';
+
+export const routes: RouteObject[] = [
+  {
+    element: (
+      <DashboardLayout>
+        <Suspense>
+          <Outlet />
+        </Suspense>
+      </DashboardLayout>
+    ),
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: '/blank',
+        element: <BlankPage />,
+      },
+      {
+        path: '/statistcs',
+        element: <Satistcs />,
+      },
+    ],
+  },
+];
